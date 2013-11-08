4a6e5cb6203f8dfc35c7c49f361b36abb47713c1
Tom Brennan <tjb1982@gmail.com>
2013-11-08 09:33:00 -0500

    removed cmp-layout-base dependency

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var base = require('cmp-layout-base'),
-    header = require('cmp-header-base'),
-    footer = require('cmp-footer-base');
+var
+hoquet = require('hoquet');
 
-module.exports = function(c) {
-  return base({
-    title: "onclickstudios" + (c.title ? " &bull; " + c.title : ''),
-    head: c.head,
-    body: ["div",
-      {class: "cmp-layout-single"},
-      header(),
-      c.body,
-      footer()]
-  });
-}
+function cmpLayoutSingle(context) {
+  return hoquet.render(
+    ['html',
+     ['head',
+      context && context.title && ['title', context.title],
+      context && context.head],
+     ['body',
+      {class:'cmp-layout-single'},
+      context && context.body]]
+  );
+  
+};
+
+module.exports = cmpLayoutSingle;
